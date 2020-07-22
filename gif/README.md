343f8e82844463fae1a29a5cf8397a4751577fde
Henry Harris <henry.harris@henryharris-ltm.internal.salesforce.com>
2020-07-22 16:37:36 -0400

    Convert videos to gif

---
+++ gif/README.md
@@ -0,0 +1,14 @@
+# Gif
+A collection of scripts to do gif things
+
+## Video to Gif
+Converts a video to a gif
+
+### Requirements
+- [FFmpeg](https://ffmpeg.org/)
+- [Gifski](https://gif.ski/)
+
+### Usage
+```
+./video_to_gif.sh video.mp4
+```
---
+++ gif/video_to_gif.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+function cleanup {
+  rm -rf temp
+}
+
+set -e
+
+video=$1
+
+if [ -z "\"$video\"" ]; then
+  echo "error: must supply a video to be converted!"
+  exit 1
+fi
+
+echo "Converting video \`$video\`"
+
+if [ -d "temp" ]; then
+  echo "error: temp directory already exists!"
+  exit 1
+fi
+
+mkdir temp
+
+echo 'converting video to frames...'
+if ! ffmpeg -i "$video" temp/frame%04d.png ; then
+  echo "error: converting video to frames failed"
+  cleanup
+  exit 1
+fi
+
+echo 'converting frames to gif...'
+if ! gifski -o output.gif temp/frame*.png ; then
+  echo "error: converting frames to gif failed"
+  cleanup
+  exit 1
+fi
+
+cleanup
+
+echo 'gif created! `output.gif`'
+exit 0
