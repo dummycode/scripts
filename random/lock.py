0544e09e4b2c1faa82ef5abe1f6379bcacf98f5c
Henry Harris <hharris@palantir.com>
2021-01-27 15:11:10 -0500

    Add locking script

---
+++ random/lock.py
@@ -0,0 +1,22 @@
+from datetime import datetime, time
+import subprocess
+
+def is_between(current_time, start, end):
+    return current_time > start and current_time < end
+
+def is_weekday(current_time):
+    return current_time.weekday() < 5
+
+current_datetime = datetime.now()
+current_time = current_datetime.time()
+
+morning, evening = time(8, 0), time(17, 0)
+if is_weekday(current_datetime) and is_between(current_time, morning, evening):
+    response = input("Have you set your status in Slack? [y/n]\n")
+    if response != "y":
+        print("Do that!")
+        exit()
+
+print("Locking...")
+subprocess.run(["pmset", "displaysleepnow"])
+
